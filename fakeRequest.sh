7f6506b55ef6bf9d656eff415d1acb2d7f892311
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-03-23 22:04:15 +0100

    delete firebase notification and fakerequest.sh

--- fakeRequest.sh
+++
@@ -1,9 +0,0 @@
-curl -XPOST -H "Content-type: application/json" -d '{
-	"mac" : 80010000,
-	"currentCpuLoad" : 100,
-	"currentDiskUsage" : 100,
-	"currentMemLoad" : 100,
-	"currentSwapUsage" : 100,
-	"currentConnectedUsers" : 30,
-	"processCounter" : 1400
-}' 'http://127.0.0.1:5000/'
