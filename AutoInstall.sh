5d26bca3056266d7f2efb96ffeeef5afc8de3c3e
Labrak Yanis <yanis.labrak.itec@gmail.com>
2019-03-24 21:27:38 +0100

    mkdir

--- AutoInstall.sh
+++ AutoInstall.sh
@@ -3,7 +3,7 @@ apt get-install python
 apt get-install pip
 pip install -r requirements.txt
 
-# mkdir -f /usr/lib/pineapple	#réaliser le test si existant ou non
+mkdir -f /usr/lib/pineapple	#réaliser le test si existant ou non
 cp CollectData.py /usr/lib/pineapple/CollectData.py
 cp AutoCheckUser.py /usr/lib/pineapple/AutoCheckUser.py
 
